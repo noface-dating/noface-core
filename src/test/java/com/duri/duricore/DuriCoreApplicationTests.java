2c7d808288342942e698863c5e414b58b169f9f8
YeonWooKimm <kyw10987@gmail.com>
2026-01-17 16:07:06 +0900

    initial commit

---
+++ src/test/java/com/duri/duricore/DuriCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duri.duricore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
