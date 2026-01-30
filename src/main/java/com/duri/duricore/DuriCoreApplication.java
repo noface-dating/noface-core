eef53a4c5ccb7bff3544cd1ceb56fb4ed52d6a3f
YeonWooKimm <kyw10987@gmail.com>
2026-01-30 21:01:51 +0900

    feat: 의존성 제거 -> spring-starter-security

--- src/main/java/com/duri/duricore/DuriCoreApplication.java
+++ src/main/java/com/duri/duricore/DuriCoreApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication(
         exclude = {
                 DataSourceAutoConfiguration.class,
                 HibernateJpaAutoConfiguration.class,
-                SecurityAutoConfiguration.class
         }
 )
 public class DuriCoreApplication {
