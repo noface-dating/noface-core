df34a2eb9c917cc38e3aae7a55c4052e6b49eba9
YeonWooKimm <kyw10987@gmail.com>
2026-01-30 20:47:52 +0900

    fix:CI/CD 정상화 및 개발용 API 테스트 환경 구성 완료

--- src/main/java/com/duri/duricore/DuriCoreApplication.java
+++ src/main/java/com/duri/duricore/DuriCoreApplication.java
@@ -1,15 +1,16 @@
 package com.duri.duricore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication(
         exclude = {
                 DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class
+                HibernateJpaAutoConfiguration.class,
+                SecurityAutoConfiguration.class
         }
 )
 public class DuriCoreApplication {
