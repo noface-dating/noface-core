5be2ade59381e285eba08e44b09d642abfb426f0
YeonWooKimm <kyw10987@gmail.com>
2026-03-23 00:39:31 +0900

    fix: .

--- src/main/java/com/duri/duricore/global/config/RedisConfig.java
+++ src/main/java/com/duri/duricore/global/config/RedisConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
 
-@Configuration("globalRedisConfig")
+@Configuration
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
