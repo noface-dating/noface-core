4977d27d3d60fe3fef86fe84f1004649553f3732
YeonWooKimm <kyw10987@gmail.com>
2026-03-23 00:33:44 +0900

    fix: globalexceptionhandler 중복 제거

--- src/main/java/com/duri/duricore/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/duri/duricore/common/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice("globalExceptionHandlerV2")
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
